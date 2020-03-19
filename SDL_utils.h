c9a337019297ddfd1d3f059e283c8ed3f3c60ae9
Huy Nguyen <nguyenduchuy1711@gmail.com>
2020-03-19 09:31:59 +0700

    test SDL

---
+++ SDL_utils.h
@@ -0,0 +1,17 @@
+#ifndef SDL_UTILS__H
+#define SDL_UTILS__H
+
+#include <SDL.h>
+#include <iostream>
+
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer,
+             int SCREEN_HEIGHT, int SCREEN_WIDTH, const std::string &WINDOW_TITLE);
+
+void logSDLError(std::ostream& os,
+                 const std::string &msg, bool fatal = false);
+
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
+
+void waitUntilKeyPressed();
+
+#endif // SDL_UTILS__H
