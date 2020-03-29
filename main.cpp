e3faf39b9055efe749a6e532b9960d595f2f1531
Huy Nguyen <nguyenduchuy1711@gmail.com>
2020-03-30 00:54:43 +0700

    Test SDL: Draw butterfly

--- main.cpp
+++ main.cpp
@@ -19,18 +19,50 @@ int main(int argc, char* argv[])
     initSDL(window, renderer, SCREEN_HEIGHT, SCREEN_WIDTH, WINDOW_TITLE);
 
     SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 0, 300, 300, 50);
-
+    SDL_SetRenderDrawColor(renderer, 0, 200, 200, 50);
 
     Painter painter(window, renderer);
-    for (int j=0; j<36; j++) {
-        for (int i=0; i<10; i++) {
+    painter.setColor(CYAN_COLOR);
+
+    painter.turnLeft(30);
+    for (int j=0; j<4; j++){
+        for (int i=0; i<4; i++) {
             painter.setRandomColor();
-            painter.moveForward(30);
+            painter.moveForward(50);
+            painter.turnRight(20);
         }
-        painter.setPosition(400, 300);
-        painter.turnRight(10);
+        painter.turnRight(100);
+        for (int i=0; i<4; i++) {
+            painter.setRandomColor();
+            painter.moveForward(50);
+            painter.turnRight(20);
+        }
+        painter.turnLeft(180);
     }
+
+    painter.setPosition(400, 300);
+    painter.setAngle(60);
+    painter.turnRight(15);
+    int i=110, rad=10;
+    do {
+        painter.setRandomColor();
+        painter.moveForward(i);
+        painter.turnRight(rad);
+        i/=2;
+        rad+=20;
+    } while(i>0);
+    painter.setPosition(400, 300);
+    painter.setAngle(60);
+    painter.turnLeft(15);
+    int i2=110, rad2=10;
+    do {
+        painter.setRandomColor();
+        painter.moveForward(i2);
+        painter.turnLeft(rad2);
+        i2/=2;
+        rad2+=20;
+    } while(i2>0);
+
 
     SDL_RenderPresent(renderer);
 
