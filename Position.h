10955df210091a447a341f12058c95b6ddd27712
Huy Nguyen <nguyenduchuy1711@gmail.com>
2020-05-11 14:25:36 +0700

    update game

--- Position.h
+++ Position.h
@@ -15,24 +15,6 @@ struct Position
         return x == p.x && y == p.y;
     }
 
-    void move(Direction direction){
-        switch(direction) {
-            case UP:
-                //if (y > 0 && !map1[y-1][x])
-                    y -= 1;
-                break;
-            case DOWN:
-                //if (y < 30-1 && !map1[y+1][x])
-                    y += 1;
-                break;
-            case LEFT:
-                //if (x > 0 && !map1[y][x-1])
-                    x -= 1; break;
-            case RIGHT:
-                //if (x < 20-1 && !map1[y][x+1])
-                x += 1; break;
-        }
-    }
 };
 
 #endif // POSITION_H
